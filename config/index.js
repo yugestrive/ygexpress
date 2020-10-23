d01efa6322a4ae7764522c82e718590496542476
yugestrive <15090221166@163.com>
2020-10-23 09:14:56 +0800

    refactor(express+mongo):优化框架，封装mongo

---
+++ config/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Autor: yugeStrive
+ * @Date: 2020-10-22 14:04:21
+ * @LastEditTime: 2020-10-22 14:26:14
+ * @Description: 全局配置文件
+ */
+export default {
+  // Mongo数据库连接地址
+  DB_URL: 'mongodb://localhost:27017',
+  // 后台服务端口号
+  PORT: 8000,
+  // 后台控制台打印样式
+  STYLE: '\x1B[36m%s\x1B[0m',
+}
